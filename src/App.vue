69cd1a90e22f47a858659646e2eedf193911fa07
CJ Karkut <cjkarkut@gmail.com>
2024-10-23 12:45:58 -0500

    create vue app

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="content">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+header {
+  position: fixed;
+}
+
+.content {
+  margin-top: 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }} - {{ item.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+  data() {
+    return {
+      items: ref([
+        { id: 1, name: 'Product 1', price: 10 },
+        { id: 2, name: 'Product 2', price: 20 },
+        { id: 3, name: 'Product 3', price: 30 },
+      ]),
+    }
+  },
+}
+</script>
